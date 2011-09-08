5e3748eb6b50a6341e6f04a87ca114250659643d
Cyrille Le Menestrel <cy_altern@yahoo.fr>
2011-09-08 22:04:13 +0000

    un point virgule manquant et hop! c'est le drame...
    git-svn-id: svn://trac.rezo.net/spip/branches/spip-1.9.2@18472 caf5f3e8-d4fe-0310-bb3e-c32d5e47d55d

--- ecrire/inc/rubriques.php
+++ ecrire/inc/rubriques.php
@@ -250,7 +250,7 @@ function calcul_branche ($generation) {
  		// normaliser $id qui a pu arriver comme un array, comme un entier, ou comme une chaine NN,NN,NN
  		if (!is_array($generation)) $generation = explode(',',$generation);
 		$generation = join(',',array_map('intval',explode(',',$generation)));
-		$branche[] = $generation
+		$branche[] = $generation;
 		while ($generation = calcul_generation ($generation))
 			$branche[] = $generation;
 		return join(",",$branche);
